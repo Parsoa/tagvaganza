4270a33502d60d6b1bc3f445554e8f2c61a79c1a
Parsoa Khorsand <parsoakhorsand91@gmail.com>
2018-04-07 00:31:04 -0700

    Update .gitignore for real

--- src/vaganza/commons.py
+++
@@ -1,7 +0,0 @@
-
-file_formats = ['MP3', 'ALAC', 'FLAC', 'M4A']
-
-# Google APIs
-# http://www.google.com/cse
-google_api_key = 'AIzaSyB9HWcXYkuVKIjTJVKK7Tby-MEEzVL7QI0'
-search_engine_name = '006860170516182512998:fcskmugwehw'
